a4137674c175a3e769685a803f52c7dc96c4e600
Pin-Han <08.pinhan@gmail.com>
2024-11-14 16:48:32 +0800

    chore: update comments in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -2,7 +2,7 @@ import time
 import pandas as pd
 from datetime import timedelta
 
-
+# here are some dicts
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
